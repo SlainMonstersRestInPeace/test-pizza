371dcf5560f0bffc7c7b134826ef46e7c05fad79
Sergey Stenyanskiy <qweartekyl@gmail.com>
2025-04-15 15:51:22 +0300

    initial commit

---
+++ src/views/main.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/Header";
+import Offer from "@/components/Offer";
+import Featured from "@/components/Featured";
+import Menu from "@/components/Menu";
+import Order from "@/components/Order";
+import About from "@/components/About";
+import Social from "@/components/Social";
+import Footer from "@/components/Footer";
+
+export default function Main() {
+  return (
+    <>
+      <Header />
+      <Offer />
+      <Featured />
+      <Menu />
+      <Order />
+      <About />
+      <Social />
+      <Footer />
+    </>
+  );
+}
