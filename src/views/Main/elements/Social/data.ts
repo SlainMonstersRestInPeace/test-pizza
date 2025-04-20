0266c7bc11d4dd9ea16177f2ad3736b60de51e6f
Sergey Stenyanskiy <qweartekyl@gmail.com>
2025-04-20 20:23:13 +0300

    feat: блок social

---
+++ src/views/Main/elements/Social/data.ts
@@ -0,0 +1,47 @@
+import soc1 from "@/assets/images/social/soc-1.jpg";
+import soc2 from "@/assets/images/social/soc-2.jpg";
+import soc3 from "@/assets/images/social/soc-3.jpg";
+import soc4 from "@/assets/images/social/soc-4.jpg";
+import soc5 from "@/assets/images/social/soc-5.jpg";
+import soc6 from "@/assets/images/social/soc-6.jpg";
+import soc7 from "@/assets/images/social/soc-7.jpg";
+import soc8 from "@/assets/images/social/soc-8.jpg";
+import soc9 from "@/assets/images/social/soc-9.jpg";
+import soc10 from "@/assets/images/social/soc-10.jpg";
+
+export interface ISocialItem {
+  imageUrl: string;
+}
+
+export const socialItems: ISocialItem[] = [
+  {
+    imageUrl: soc1,
+  },
+  {
+    imageUrl: soc2,
+  },
+  {
+    imageUrl: soc3,
+  },
+  {
+    imageUrl: soc4,
+  },
+  {
+    imageUrl: soc5,
+  },
+  {
+    imageUrl: soc6,
+  },
+  {
+    imageUrl: soc7,
+  },
+  {
+    imageUrl: soc8,
+  },
+  {
+    imageUrl: soc9,
+  },
+  {
+    imageUrl: soc10,
+  },
+];
--- src/views/Main/elements/Social/index.tsx
+++ src/views/Main/elements/Social/index.tsx
@@ -1,3 +1,21 @@
+import Typography from "@/primitives/Typography";
+import { socialItems } from "./data";
+
 export default function Social() {
-  return <div className="social"></div>;
+  return (
+    <div className="social">
+      <div className="social__invitation">
+        <Typography variant="Heading-2">Следите за нами в Instagram</Typography>
+        {/* TODO */}
+        <Typography variant="Heading-4" color="#848A9A" as="a">
+          @pizzamenu
+        </Typography>
+      </div>
+      <div className="social__inst-items">
+        {socialItems.map(({ imageUrl }) => (
+          <img src={imageUrl} className="social__inst-item" />
+        ))}
+      </div>
+    </div>
+  );
 }
